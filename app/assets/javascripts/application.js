30ab76ccd276ea78c227f8f8359fd5fbf40cd5dd
Eric Katz <ericpkatz@gmail.com>
2014-03-02 19:47:35 -0500

    add js files for marionette application

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,7 @@
 // about supported directives.
 //
 //= require jquery
-//= require jquery_ujs
-//= require turbolinks
-//= require_tree .
+//= require bootstrap
+//= require lib/underscore
+//= require lib/backbone
+//= require lib/backbone.marionette
